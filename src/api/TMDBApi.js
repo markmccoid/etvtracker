0c21dddf797c3a4f68a74a69a637941248bfc2e9
Mark McCoid <markmccoid@gmail.com>
2018-10-01 00:09:27 -0400

    update jsdocs

--- src/api/TMDBApi.js
+++ src/api/TMDBApi.js
@@ -3,30 +3,50 @@ import axios from 'axios';
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const API_URL = 'https://api.themoviedb.org/3';
 
-// ========================================
+/**
+ * Returns configuration information from TMDb.
+ * 
+ * @returns {object} response object {data, msg} 
+ *  on success { data: data from api call, apiCall: API call}
+ *  on error { data: 'ERROR', msg: error message, }
+ */
 export const getConfig = () => {
+  const apiCall = `${API_URL}/configuration?api_key=${API_KEY}`;
   return axios
-    .get(`${API_URL}/configuration?api_key=${API_KEY}`)
+    .get(apiCall)
       .then((resp) => {
         return {
           data: resp.data,
-          msg: resp.request.responseURL
+          apiCall: resp.request.responseURL
         }
       })
       .catch((err) => {
         console.log(`Error with config get: ${err}`)
-        return err;
+        return {
+          data: 'ERROR',
+          apiCall,
+          msg: err
+        };
       });
 };
 
-// ========================================
+/**
+ * Returns data from search by searchString
+ * 
+ * @param {string} searchString - String of title to search for
+ * @param {number} [page=1] - page number to return if multiple pages from search 
+ * @returns {object} response object {data, msg} 
+ *  on success { data: data from api call, apiCall: API call}
+ *  on error { data: 'ERROR', msg: error message, }
+ */
 export const searchTVByTitle = (searchString, page=1) => {
+  const apiCall = `${API_URL}/search/tv?api_key=${API_KEY}&page=${page}&include_adult=false&query=${searchString}`; 
   return axios
-    .get(`${API_URL}/search/tv?api_key=${API_KEY}&page=${page}&include_adult=false&query=${searchString}`)
+    .get(apiCall)
       .then((resp) => {
         return {
           data: resp.data,
-          msg: resp.request.responseURL
+          apiCall: resp.request.responseURL
         }
       })
       .catch((err) => {
@@ -35,14 +55,22 @@ export const searchTVByTitle = (searchString, page=1) => {
       });
 };
 
-// ========================================
+/**
+ * Returns show details for passed TMDb showId
+ * 
+ * @param {string} showId - TMDb show id
+ * @returns {object} response object {data, msg} 
+ *  on success { data: data from api call, apiCall: API call}
+ *  on error { data: 'ERROR', msg: error message, }
+ */
 export const getShowDetails = (showId) => {
+  const apiCall = `${API_URL}/tv/${showId}?api_key=${API_KEY}`;
   return axios
-    .get (`${API_URL}/tv/${showId}?api_key=${API_KEY}`)
+    .get (apiCall)
       .then((resp) => {
         return {
           data: resp.data,
-          msg: resp.request.responseURL
+          apiCall: resp.request.responseURL
         };
       })
       .catch((err) => {
@@ -51,38 +79,63 @@ export const getShowDetails = (showId) => {
       });
 };
 
-// ========================================
+/**
+ * Return episodes from showId passed and seasonNum passed
+ * 
+ * @param {string} showId - TMDb show id
+ * @param {number} seasonNum - season number to get episodes from
+ * @returns {object} response object {data, msg} 
+ *  on success { data: data from api call, apiCall: API call}
+ *  on error { data: 'ERROR', msg: error message, }
+ */
 export const getEpisodes = (showId, seasonNum) => {
+  const apiCall = `${API_URL}/tv/${showId}/season/${seasonNum}?api_key=${API_KEY}`;
   return axios
-    .get(`${API_URL}/tv/${showId}/season/${seasonNum}?api_key=${API_KEY}`)
+    .get(apiCall)
       .then((resp) => {
         return {
           data: resp.data,
-          msg: resp.request.responseURL
+          apiCall: resp.request.responseURL
         }
     });
 };
 
-// ========================================
+/**
+ * Returns show images for passed showId from TMDb.
+ * 
+ * @param {string} showId - TMDb show id
+ * @returns {object} response object {data, msg} 
+ *  on success { data: data from api call, apiCall: API call}
+ *  on error { data: 'ERROR', msg: error message, }
+ */
 export const getShowImages = (showId) => {
+  const apiCall = `${API_URL}/tv/${showId}/images?api_key=${API_KEY}`;
   return axios
-    .get(`${API_URL}/tv/${showId}/images?api_key=${API_KEY}`)
+    .get(apiCall)
       .then((resp) => {
         return {
           data: resp.data,
-          msg: resp.request.responseURL
+          apiCall: resp.request.responseURL
         }
       });
 };
 
-// ========================================
+/**
+ * Returns external Ids from TMDb.
+ * 
+ * @param {string} showId - TMDb show id
+ * @returns {object} response object {data, msg} 
+ *  on success { data: data from api call, apiCall: API call}
+ *  on error { data: 'ERROR', msg: error message, }
+ */
 export const getExternalIds = (showId) => {
+  const apiCall = `${API_URL}/tv/${showId}/external_ids?api_key=${API_KEY}`;
   return axios
-    .get(`${API_URL}/tv/${showId}/external_ids?api_key=${API_KEY}`)
+    .get(apiCall)
       .then((resp) => {
         return {
           data: resp.data,
-          msg: resp.request.responseURL
+          apiCall: resp.request.responseURL
         }
       })
 }
